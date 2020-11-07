10384d8a6d17738a8636d9d18ecdef1062a3f113
david.batet <david.batet@estudiantat.upc.edu>
2020-11-07 22:38:59 +0100

    Created Specimen, added generalized readSpecimen.

--- dts.hs
+++ dts.hs
@@ -1,10 +1,13 @@
 main :: IO ()
-main = putStrLn "Decision trees."
+main = do
+    contents <- readFile "agaricus-lepiota.data"
+    putStrLn contents
 
 {-Example to run
 Node "cap-color" [("brown",Leaf "poisonous"),("yellow",Leaf "edible"),("white",Node "cap-shape" [("bell",Leaf "edible"),("convex",Leaf "poisonous")])]
 -}
 
+data Specimen a b = Specimen a [b] deriving Show
 data DT a b = Leaf a | Node String [(b, DT a b)]
 
 -- Creates a String only containing the given number of spaces
@@ -18,3 +21,18 @@ instance (Show a, Show b) => Show (DT a b) where
         show' n (Node node list) =
             spaces n ++ show node ++ ['\n'] ++ concatMap (show'' (n + 2)) list where
                 show'' m (branch, dt') = spaces m ++ show branch ++ ['\n'] ++ show' (m + 2) dt'
+
+-- The words function generalized so that it takes the character separing each
+--  of the words as an argument
+wordsCustom :: Char -> String -> [String]
+wordsCustom sep str = words $ map replace str
+  where
+    replace char
+        | char == sep   = ' '
+        | otherwise     = char
+
+-- Reads a Specimen from a String, whose elements are separed by the character sep
+readSpecimen :: (Read a, Read b) => Char -> String -> Specimen a b
+readSpecimen sep str = Specimen (read $ head splitStr) (map read (tail splitStr))
+  where
+    splitStr = wordsCustom sep str
