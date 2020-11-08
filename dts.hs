7adce84a15cdcab7562ca7eefb5f394ea9591ad9
david.batet <david.batet@estudiantat.upc.edu>
2020-11-08 18:42:03 +0100

    Created function returning the accuracy list.

--- dts.hs
+++ dts.hs
@@ -1,10 +1,15 @@
+--So as to use group and sort
+import           Data.List
+
 main :: IO ()
 main = do
     contents <- readFile "agaricus-lepiota.data"
     putStrLn contents
 
-{-Example to run
+{-Examples to run
 Node "cap-color" [("brown",Leaf "poisonous"),("yellow",Leaf "edible"),("white",Node "cap-shape" [("bell",Leaf "edible"),("convex",Leaf "poisonous")])]
+[Specimen "poisonous" ["convex", "brown", "black"], Specimen "edible" ["convex", "yellow", "black"], Specimen "edible" ["bell", "white", "brown"], Specimen "poisonous" ["convex", "white", "brown"], Specimen "edible" ["convex", "yellow", "brown"], Specimen "edible" ["bell", "white", "brown"], Specimen "poisonous" ["convex", "white", "pink"]]
+[Specimen "edible" ["bell", "brown"], Specimen "poisonous" ["convex", "brown"], Specimen "edible" ["bell", "brown"], Specimen "poisonous" ["convex", "pink"]]
 -}
 
 data Specimen a b = Specimen a [b] deriving Show
@@ -55,6 +60,50 @@ readSpecimenCC sep str = Specimen (unpack $ head splitStr) (map unpack $ tail sp
     unpack [x] = x  --We could have used head instead, but it would have matched
                     -- Strings that don't have only one character, potentially
                     -- giving unexpected behavior in other parts of the program.
+
+
+spClass :: Specimen a b -> a
+spClass (Specimen x _) = x
+
+
+--(Eq a) suffices, using (Ord a) is a too strong condition
+--Returns the most common element in a list
+mostCommon :: (Ord a) => [a] -> a
+mostCommon xs = snd $ maximum (gathered xs)
+    where gathered = map (\ys -> (length ys, head ys)) . group . sort
+
+
+--Creates a list in which every element is a pair that contains one of the
+-- attribute id's provided in 'unused', and a sublist made out of triplets
+-- (value, appearances, class), where
+-- 1. value: is one of the values of the attribute corresponding to the id.
+-- 2. class: is one of the possible classes.
+-- 3. appearances: is the number of appearances of the combination class-value
+--   in the list of Specimens provided.
+appsList :: (Ord a, Ord b) => [Int] -> [Specimen a b] -> [(Int, [(b, Int, a)])]
+appsList unused sps = map gathered unused
+  where
+    gathered attrId = (attrId, sortBy customOrder $ map valNumClassTriplet (groupedBag attrId))
+    valNumClassTriplet zs = (fst $ head zs, length zs, snd $ head zs)
+    groupedBag attrId = group $ sort $ map (classValPair attrId) sps
+    classValPair attrId (Specimen x ys) = (ys !! attrId, x)
+    customOrder (val1, app1, _) (val2, app2, _)
+        | val1 < val2                       = LT
+        | (val1 == val2) && (app1 >= app2)  = LT
+        | otherwise                         = GT
+
+-- Accuracy list
+accList :: (Ord a, Ord b) => [(Int, [(b, Int, a)])] -> [(Int, Int)]
+accList ws = sortBy (\(x, _) (y, _) -> compare y x) $ map whatever ws
+  where
+    whatever (attrId, zs) = (countFirstApp zs, attrId)
+    countFirstApp []                  = 0
+    countFirstApp ((val, app, _):zs') = countFirstApp' zs' val app
+    countFirstApp' [] _ n = n
+    countFirstApp' ((newval, app, _):zs') val n
+        | newval == val   = countFirstApp' zs' val n
+        | otherwise       = countFirstApp' zs' newval (n + app)
+
 
 --generateDT :: [Specimen a b] -> DT a b
 --generateDT sps = generateDT' [] sps
