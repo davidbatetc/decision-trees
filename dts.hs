f4ca1958a661f6a99b27cdcc2459faa771563f06
david.batet <david.batet@estudiantat.upc.edu>
2020-11-19 12:56:00 +0100

    Missing attribute name from "agaricus-lepiota.data" added.

--- dts.hs
+++ dts.hs
@@ -251,7 +251,7 @@ main = do
         attrNames = ["cap-shape", "cap-surface", "cap-color"
             , "bruises?", "odor", "gill-attachment", "gill-spacing"
             , "gill-size", "gill-color", "stalk-shape"
-            , "stalk-root", "stalk-surface-bove-ring"
+            , "stalk-root", "stalk-surface-above-ring", "stalk-surface-below-ring"
             , "stalk-color-above-ring", "stalk-color-below-ring"
             , "veil-type", "veil-color", "ring-number", "ring-type"
             , "spore-print-color", "population", "habitat"]
